3daec7763ea6c560041940c9f82b5b58e2b495d3
Alexander Shabanov <avshabanov@gmail.com>
2015-02-23 00:39:45 -0800

    RestServiceBinder: draft implementation completed

---
+++ brikar-client/src/main/java/com/truward/brikar/client/binder/support/StandardRestServiceBinder.java
@@ -0,0 +1,404 @@
+package com.truward.brikar.client.binder.support;
+
+import com.truward.brikar.client.binder.RestServiceBinder;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriTemplate;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+/**
+ * @author Alexander Shabanov
+ */
+public class StandardRestServiceBinder implements RestServiceBinder {
+  private final RestOperations restOperations;
+
+  public StandardRestServiceBinder(@Nonnull RestOperations restOperations) {
+    Assert.notNull(restOperations, "restOperations");
+    this.restOperations = restOperations;
+  }
+
+  @Override
+  @Nonnull
+  public <T> T createClient(@Nonnull String serviceBaseUrl, @Nonnull Class<T> restServiceClass) {
+    final Map<Method, MethodInvocationHandler> handlerMap = new HashMap<>();
+    for (final Method method : restServiceClass.getMethods()) {
+      handlerMap.put(method, getMethodInvocationHandler(method, restServiceClass, serviceBaseUrl));
+    }
+
+    final Object o = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+        new Class[]{restServiceClass}, new MethodMapBasedInvocationHandler(handlerMap));
+    return restServiceClass.cast(o);
+  }
+
+  //
+  // Private
+  //
+
+  /*
+   * Creates an instance of an interface which is supposed to handle method calls in the proxy.
+   */
+  @Nonnull
+  private MethodInvocationHandler getMethodInvocationHandler(@Nonnull Method method,
+                                                             @Nonnull final Class<?> clazz,
+                                                             @Nonnull String serviceBaseUrl) {
+    // toString+hashCode+equals
+    if (method.getName().equals("toString") && method.getParameterTypes().length == 0) {
+      return new MethodInvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+          return clazz.getSimpleName();
+        }
+      };
+    } else if (method.getName().equals("hashCode") && method.getParameterTypes().length == 0) {
+      return new MethodInvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+          return this.hashCode();
+        }
+      };
+    } else if (method.getName().equals("equals") && method.getParameterTypes().length == 1 &&
+        method.getParameterTypes()[0] == Object.class) {
+      return new MethodInvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+          return proxy == args[0];
+        }
+      };
+    }
+
+    return createRequestMappingHandler(method, serviceBaseUrl);
+  }
+
+  private interface MethodInvocationHandler {
+    Object invoke(Object proxy, Method method, Object[] args) throws Throwable;
+  }
+
+  private static class MethodMapBasedInvocationHandler implements InvocationHandler {
+    final Map<Method, MethodInvocationHandler> handlers;
+
+    public MethodMapBasedInvocationHandler(@Nonnull Map<Method, MethodInvocationHandler> handlers) {
+      this.handlers = handlers;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+      final MethodInvocationHandler handler = handlers.get(method);
+      if (handler == null) {
+        throw new UnsupportedOperationException("Unsupported method=" + method);
+      }
+      return handler.invoke(proxy, method, args);
+    }
+  }
+
+  @Nonnull
+  private MethodInvocationHandler createRequestMappingHandler(@Nonnull Method method, @Nonnull String serviceBaseUrl) {
+
+    final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+    if (requestMapping == null) {
+      throw new BeanInitializationException("RequestMapping annotation is missing");
+    }
+
+    checkUnsupportedProperties(requestMapping);
+    final RequestMethod requestMethod = getRequestMethod(requestMapping);
+    final MethodParseResult parseResult = parseMethod(method, serviceBaseUrl, requestMapping);
+
+    return new RestMethodInvocationHandler(getHttpMethod(requestMethod),
+        parseResult.uriExtractor, parseResult.bodyExtractor, restOperations);
+  }
+
+  @Nonnull
+  private HttpMethod getHttpMethod(@Nonnull RequestMethod requestMethod) {
+    switch (requestMethod) {
+      case GET:
+        return HttpMethod.GET;
+      case DELETE:
+        return HttpMethod.DELETE;
+      case HEAD:
+        return HttpMethod.HEAD;
+      case OPTIONS:
+        return HttpMethod.OPTIONS;
+      case POST:
+        return HttpMethod.POST;
+      case PUT:
+        return HttpMethod.PUT;
+      case TRACE:
+        return HttpMethod.TRACE;
+      case PATCH:
+        return HttpMethod.PATCH;
+      default:
+        throw new IllegalArgumentException("Invalid HTTP method: " + requestMethod);
+    }
+  }
+
+  @Nonnull
+  private MethodParseResult parseMethod(@Nonnull Method method,
+                                        @Nonnull String serviceBaseUrl,
+                                        @Nonnull RequestMapping requestMapping) {
+    final Annotation[][] methodParamAnnotations = method.getParameterAnnotations();
+
+    final List<PosNamePair> posNamePairs = new ArrayList<>(10);
+    final String prefixUrl = getRequestMappingPrefixUrl(requestMapping);
+    final StringBuilder builder = new StringBuilder(serviceBaseUrl.length() + prefixUrl.length() + 50);
+    RequestBodyExtractor bodyExtractor = EmptyRequestBodyExtractor.INSTANCE;
+
+    builder.append(serviceBaseUrl).append(prefixUrl);
+
+    // iterate over each method parameter's annotation
+    for (int i = 0, rp = 0; i < methodParamAnnotations.length; ++i) {
+      final Annotation[] annotations = methodParamAnnotations[i];
+      for (final Annotation a : annotations) {
+        if (a.annotationType().equals(PathVariable.class)) {
+          posNamePairs.add(new PosNamePair(i, ((PathVariable) a).value()));
+          continue;
+        } else if (a.annotationType().equals(RequestParam.class)) {
+          final String name = ((RequestParam) a).value();
+          posNamePairs.add(new PosNamePair(i, name));
+
+          // since this is a request param we need to modify URL template as well
+          // this is needed to make UriTemplate handle query parameters in addition to path parameters
+          builder.append(rp > 0 ? '&' : '?').append(name).append('=').append(getParamMask(builder.toString(), name));
+
+          ++rp;
+          continue;
+        } else if (a.annotationType().equals(RequestBody.class)) {
+          if (bodyExtractor != EmptyRequestBodyExtractor.INSTANCE) {
+            // more than one request body? - possible wrong mapping
+            throw new UnsupportedOperationException("Can not have more than one RequestBody, offending " +
+                "parameter number: " + (i + 1));
+          }
+
+          bodyExtractor = new PosArgBodyExtractor(i);
+          continue;
+        }
+
+        throw new UnsupportedOperationException("Unable to create method handler because of parameter number "  +
+            (i + 1) + ": PathVariable, RequestParam or RequestBody annotation is expected");
+      }
+    }
+
+    final UriExtractor uriExtractor;
+    if (posNamePairs.isEmpty()) {
+      // optimized version
+      uriExtractor = new EmptyParamsUriExtractor(builder.toString());
+    } else {
+      uriExtractor = new StandardUriExtractor(builder.toString(), posNamePairs.toArray(new PosNamePair[posNamePairs.size()]));
+    }
+
+    return new MethodParseResult(uriExtractor, bodyExtractor);
+  }
+
+  private static String getParamMask(@Nonnull String partialUrl, @Nonnull String argName) {
+    final StringBuilder mask = new StringBuilder(argName.length() + 3);
+    for (int i = 0;; ++i) {
+      mask.setLength(0);
+      mask.append('{').append(argName);
+      if (i > 0) {
+        mask.append(i);
+      }
+      mask.append('}');
+
+      if (!partialUrl.contains(mask)) {
+        return mask.toString();
+      }
+    }
+  }
+
+  private static final class MethodParseResult {
+    final UriExtractor uriExtractor;
+    final RequestBodyExtractor bodyExtractor;
+
+    public MethodParseResult(@Nonnull UriExtractor uriExtractor, @Nonnull RequestBodyExtractor bodyExtractor) {
+      this.uriExtractor = uriExtractor;
+      this.bodyExtractor = bodyExtractor;
+    }
+  }
+
+  @Nonnull
+  private String getRequestMappingPrefixUrl(@Nonnull RequestMapping requestMapping) {
+    final String[] mappings = requestMapping.value();
+    if (mappings.length == 1) {
+      return mappings[0];
+    } else if (mappings.length == 0) {
+      return "";
+    }
+
+    throw new UnsupportedOperationException("RequestMapping.value is not expected to have more than one value");
+  }
+
+  private void checkUnsupportedProperties(@Nonnull RequestMapping requestMapping) {
+    if (requestMapping.consumes().length > 0) {
+      throw new UnsupportedOperationException("RequestMapping.consumes is not supported");
+    }
+
+    if (requestMapping.headers().length > 0) {
+      throw new UnsupportedOperationException("RequestMapping.headers is not supported");
+    }
+
+    if (requestMapping.produces().length > 0) {
+      throw new UnsupportedOperationException("RequestMapping.consumes is not supported");
+    }
+
+    if (requestMapping.params().length > 0) {
+      throw new UnsupportedOperationException("RequestMapping.params is not supported");
+    }
+  }
+
+  @Nonnull
+  private RequestMethod getRequestMethod(@Nonnull RequestMapping requestMapping) {
+    final RequestMethod[] methods = requestMapping.method();
+    if (methods.length == 1) {
+      return methods[0];
+    } else if (methods.length == 0) {
+      return RequestMethod.GET; // default method
+    }
+
+    throw new UnsupportedOperationException("RequestMapping.methods contains more than one parameters");
+  }
+
+  private static final class RestMethodInvocationHandler implements MethodInvocationHandler {
+    final HttpMethod httpMethod;
+    final UriExtractor uriExtractor;
+    final RequestBodyExtractor bodyExtractor;
+    final RestOperations restOperations;
+
+    public RestMethodInvocationHandler(@Nonnull HttpMethod httpMethod,
+                                       @Nonnull UriExtractor uriExtractor,
+                                       @Nonnull RequestBodyExtractor bodyExtractor,
+                                       @Nonnull RestOperations restOperations) {
+      this.httpMethod = httpMethod;
+      this.uriExtractor = uriExtractor;
+      this.bodyExtractor = bodyExtractor;
+      this.restOperations = restOperations;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+      final URI uri = uriExtractor.extract(args);
+      final Object body = bodyExtractor.extract(args);
+
+      HttpEntity<?> entity;
+      if (body == null) {
+        entity = HttpEntity.EMPTY;
+      } else {
+        entity = new HttpEntity<>(body);
+      }
+
+      final ResponseEntity<?> responseEntity = restOperations.exchange(uri, httpMethod, entity, method.getReturnType());
+      if (responseEntity == null) {
+        throw new IllegalStateException("responseEntity is null"); // should not happen
+      }
+      return responseEntity.getBody();
+    }
+  }
+
+  //
+  // RequestBodyExtractor
+  //
+
+  interface RequestBodyExtractor {
+    @Nullable
+    Object extract(@Nonnull Object[] arguments);
+  }
+
+  private static final class EmptyRequestBodyExtractor implements RequestBodyExtractor {
+    static final EmptyRequestBodyExtractor INSTANCE = new EmptyRequestBodyExtractor();
+
+    @Nullable
+    @Override
+    public Object extract(@Nonnull Object[] arguments) {
+      return null;
+    }
+  }
+
+  private static final class PosArgBodyExtractor implements RequestBodyExtractor {
+    final int argPos;
+
+    public PosArgBodyExtractor(int argPos) {
+      this.argPos = argPos;
+    }
+
+    @Nullable
+    @Override
+    public Object extract(@Nonnull Object[] arguments) {
+      return arguments[argPos];
+    }
+  }
+
+  //
+  // UriExtractor
+  //
+
+  interface UriExtractor {
+    @Nonnull
+    URI extract(@Nonnull Object[] arguments);
+  }
+
+  private static abstract class AbstractUriExtractor {
+    final String baseUrl;
+
+    public AbstractUriExtractor(@Nonnull String baseUrl) {
+      this.baseUrl = baseUrl;
+    }
+  }
+
+  private static final class EmptyParamsUriExtractor extends AbstractUriExtractor implements UriExtractor {
+
+    public EmptyParamsUriExtractor(@Nonnull String baseUri) {
+      super(baseUri);
+    }
+
+    @Nonnull
+    @Override
+    public URI extract(@Nonnull Object[] arguments) {
+      try {
+        return new URI(baseUrl);
+      } catch (URISyntaxException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+  }
+
+  private static final class StandardUriExtractor extends AbstractUriExtractor implements UriExtractor {
+    final PosNamePair[] variables;
+
+    public StandardUriExtractor(@Nonnull String baseUri, @Nonnull PosNamePair[] variables) {
+      super(baseUri);
+      this.variables = variables;
+    }
+
+    @Nonnull
+    @Override
+    public URI extract(@Nonnull Object[] arguments) {
+      final UriTemplate uriTemplate = new UriTemplate(baseUrl);
+      final Map<String, Object> params = new HashMap<>(variables.length * 2);
+      for (final PosNamePair pair : variables) {
+        params.put(pair.argName, arguments[pair.argPos]);
+      }
+      return uriTemplate.expand(params);
+    }
+  }
+
+  private static final class PosNamePair {
+    final int argPos;
+    final String argName;
+
+    public PosNamePair(int argPos, String argName) {
+      this.argPos = argPos;
+      this.argName = argName;
+    }
+  }
+}
--- brikar-client/src/test/java/com/truward/brikar/client/test/binder/RestServiceBinderTest.java
+++ brikar-client/src/test/java/com/truward/brikar/client/test/binder/RestServiceBinderTest.java
@@ -1,19 +1,57 @@
 package com.truward.brikar.client.test.binder;
 
+import com.truward.brikar.client.binder.RestServiceBinder;
+import com.truward.brikar.client.binder.support.StandardRestServiceBinder;
 import com.truward.brikar.client.test.fixture.ProfileModel;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
- * TODO: move to brikar-client.
  * @author Alexander Shabanov
  */
 public class RestServiceBinderTest {
 
   @Test
-  public void shouldBindRestService() {
-    // TODO: impl
+  public void shouldBindRestService() throws URISyntaxException {
+    // Given:
+    final RestOperations restOperations = mock(RestOperations.class);
+
+    final ProfileModel.Profile aGotProfile = ProfileModel.Profile.newBuilder().setId(1).setName("2").build();
+    when(restOperations.exchange(new URI("http://127.0.0.1/profile/1"), HttpMethod.GET, new HttpEntity<>(null), ProfileModel.Profile.class))
+        .thenReturn(new ResponseEntity<>(aGotProfile, HttpStatus.OK));
+
+    final ProfileModel.Profile aFoundProfile = ProfileModel.Profile.newBuilder().setId(3).setName("4").build();
+    when(restOperations.exchange(new URI("http://127.0.0.1/profile?query=test&limit=10"), HttpMethod.GET, new HttpEntity<>(null), ProfileModel.Profile.class))
+        .thenReturn(new ResponseEntity<>(aFoundProfile, HttpStatus.OK));
+
+    when(restOperations.exchange(new URI("http://127.0.0.1/profile"), HttpMethod.PUT, new HttpEntity<>(aGotProfile), Void.TYPE))
+        .thenReturn(new ResponseEntity<Void>(HttpStatus.CREATED));
+
+    final RestServiceBinder binder = new StandardRestServiceBinder(restOperations);
+    final ProfileRestService service = binder.createClient("http://127.0.0.1", ProfileRestService.class);
+
+    // When:
+    final ProfileModel.Profile gotProfile = service.getProfile(1);
+    final ProfileModel.Profile foundProfile = service.searchProfile("test", 10);
+    service.saveProfile(gotProfile);
+
+    // Then:
+    assertEquals(aGotProfile, gotProfile);
+    assertEquals(aFoundProfile, foundProfile);
   }
 
 
@@ -25,7 +63,7 @@ public class RestServiceBinderTest {
 
     @RequestMapping("/profile")
     @ResponseBody
-    ProfileModel.Profile searchProfile(@RequestParam("asd") String value);
+    ProfileModel.Profile searchProfile(@RequestParam("query") String value, @RequestParam("limit") int limit);
 
     @RequestMapping(value = "/profile", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.NO_CONTENT)
