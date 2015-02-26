81f33fb097f965b6da4c04f976f5f7c306d456a6
Alexander Shabanov <avshabanov@gmail.com>
2015-02-26 00:37:58 -0800

    Adding protobuf json reader

--- brikar-protobuf/brikar-protobuf-jackson/src/main/java/com/truward/brikar/protobuf/jackson/ProtobufJacksonUtil.java
+++ brikar-protobuf/brikar-protobuf-jackson/src/main/java/com/truward/brikar/protobuf/jackson/ProtobufJacksonUtil.java
@@ -1,6 +1,9 @@
 package com.truward.brikar.protobuf.jackson;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
@@ -20,6 +23,23 @@ import java.lang.reflect.Method;
 public final class ProtobufJacksonUtil {
   private ProtobufJacksonUtil() {} // hidden
 
+  @Nonnull
+  public static <T extends Message> T readJson(@Nonnull Class<T> messageClass, @Nonnull JsonParser jp) throws IOException {
+    final Message message;
+    try {
+      message = getDefaultInstance(messageClass);
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      throw new IOException("Reflection Error: unable to read an instance of " + messageClass, e);
+    }
+
+    // prepare message data
+    final Descriptors.Descriptor descriptor = message.getDescriptorForType();
+
+    // construct the result object
+    jp.nextToken();
+    final Message result = readMessage(descriptor, jp);
+    return messageClass.cast(result);
+  }
 
   public static void writeJson(@Nonnull Message message, @Nonnull JsonGenerator jg) throws IOException {
     final Descriptors.Descriptor descriptor = message.getDescriptorForType();
@@ -82,17 +102,88 @@ public final class ProtobufJacksonUtil {
   }
 
   @Nonnull
-  public static Descriptors.Descriptor getDescriptorForClass(@Nonnull Class<?> clazz)
+  public static Message getDefaultInstance(@Nonnull Class<?> messageClass)
       throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
 
     // use reflection to get descriptor
-    final Method method = clazz.getMethod("getDescriptor");
-    final Object descriptor = method.invoke(null);
+    final Method method = messageClass.getMethod("getDefaultInstance");
+    final Object message = method.invoke(null);
     try {
-      return (Descriptors.Descriptor) descriptor;
+      return (Message) message;
     } catch (ClassCastException e) {
-      throw new IOException("Unable to convert an instance of the returned descriptor: " +
-          "non-protobuf-generated class?", e);
+      throw new IOException("Non-protobuf-generated class: " + messageClass, e);
+    }
+  }
+
+  //
+  // Private
+  //
+
+
+
+  @Nonnull
+  private static Message readMessage(@Nonnull Descriptors.Descriptor descriptor, @Nonnull JsonParser jp) throws IOException {
+    final Message.Builder builder = descriptor.getOptions().newBuilderForType();
+
+    // make sure current token is the start object
+    JsonToken token = jp.getCurrentToken();
+    if (token != JsonToken.START_OBJECT) {
+      throw new JsonParseException("Object expected", jp.getCurrentLocation());
+    }
+
+    // read fields
+    for (token = jp.nextToken(); token != JsonToken.END_OBJECT; token = jp.nextToken()) {
+      if (token != JsonToken.FIELD_NAME) {
+        throw new JsonParseException("Field name expected", jp.getCurrentLocation()); // unlikely
+      }
+
+      // get field name
+      final String fieldName = jp.getText();
+
+      // advance to the next token (field value)
+      jp.nextToken();
+
+      final Descriptors.FieldDescriptor field = descriptor.findFieldByName(fieldName);
+      if (field == null) {
+        // TODO: handle unknown fields
+        throw new JsonParseException("Unknown field " + fieldName, jp.getCurrentLocation());
+      }
+
+      final Object object = readObject(field, jp);
+      builder.setField(field, object);
+    }
+
+    // construct the result object
+    final Message result = builder.build();
+    if (result == null) {
+      throw new IllegalStateException("Builder failed to construct a message of type " + descriptor); // unlikely
+    }
+    return result;
+  }
+
+  @Nonnull
+  private static Object readObject(@Nonnull Descriptors.FieldDescriptor descriptor, @Nonnull JsonParser jp) throws IOException {
+    switch (descriptor.getJavaType()) {
+      case INT:case LONG:case FLOAT:case DOUBLE:
+        return jp.getNumberValue();
+
+      case BOOLEAN:
+        return jp.getBooleanValue();
+
+      case STRING:
+        return jp.getText();
+
+      case BYTE_STRING:
+        return jp.getBinaryValue();
+
+      case ENUM:
+        throw new UnsupportedOperationException();
+
+      case MESSAGE:
+        throw new UnsupportedOperationException();
+
+      default:
+        throw new IOException("Unknown descriptor=" + descriptor);
     }
   }
 }
--- brikar-protobuf/brikar-protobuf-jackson/src/test/java/com/truward/brikar/protobuf/jackson/ProtobufJacksonUtilTest.java
+++ brikar-protobuf/brikar-protobuf-jackson/src/test/java/com/truward/brikar/protobuf/jackson/ProtobufJacksonUtilTest.java
@@ -2,28 +2,29 @@ package com.truward.brikar.protobuf.jackson;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.truward.brikar.protobuf.test.AddressBookModel;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 import static org.junit.Assert.assertNotNull;
 
-public class ProtobufJacksonUtilTest {
+public final class ProtobufJacksonUtilTest {
+  private final AddressBookModel.Person person = AddressBookModel.Person.newBuilder()
+      .setId(1).setName("n").setEmail("e")
+      .addPhone(AddressBookModel.Person.PhoneNumber.newBuilder()
+          .setType(AddressBookModel.Person.PhoneType.HOME)
+          .setNumber("111")
+          .build())
+      .build();
 
   @Test
   public void shouldWriteAsJson() throws IOException {
     // Given:
-    final AddressBookModel.Person person = AddressBookModel.Person.newBuilder()
-        .setId(1).setName("n").setEmail("e")
-        .addPhone(AddressBookModel.Person.PhoneNumber.newBuilder()
-            .setType(AddressBookModel.Person.PhoneType.HOME)
-            .setNumber("111")
-            .build())
-        .build();
+    final AddressBookModel.Person person = this.person;
 
     // When:
     final ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -36,6 +37,27 @@ public class ProtobufJacksonUtilTest {
     assertNotNull(json);
   }
 
+  @Ignore
+  @Test
+  public void shouldReadJson() throws IOException {
+    // Given:
+    final AddressBookModel.Person person = this.person;
+
+    // When:
+    final ByteArrayOutputStream os = new ByteArrayOutputStream();
+    try (JsonGenerator jg = jsonGenerator(os)) {
+      ProtobufJacksonUtil.writeJson(person, jg);
+    }
+
+    final AddressBookModel.Person p2;
+    final byte[] b = os.toByteArray();
+    try (JsonParser jp = jsonParser(new ByteArrayInputStream(b))) {
+      p2 = ProtobufJacksonUtil.readJson(AddressBookModel.Person.class, jp);
+    }
+
+    assertNotNull(p2);
+  }
+
   //
   // Private
   //
@@ -44,5 +66,11 @@ public class ProtobufJacksonUtilTest {
   private static JsonGenerator jsonGenerator(@Nonnull OutputStream outputStream) throws IOException {
     final JsonFactory jsonFactory = new JsonFactory();
     return jsonFactory.createGenerator(outputStream);
+  }
+
+  @Nonnull
+  private static JsonParser jsonParser(@Nonnull InputStream inputStream) throws IOException {
+    final JsonFactory jsonFactory = new JsonFactory();
+    return jsonFactory.createParser(inputStream);
   }
 }
