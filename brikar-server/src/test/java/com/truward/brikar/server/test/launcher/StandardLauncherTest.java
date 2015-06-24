c39907f84f3d99115491131ee5fe08cfc826b902
Alexander Shabanov <avshabanov@gmail.com>
2015-06-23 23:13:33 -0700

    More tests + removing simple slf4j as default logger for tests

--- brikar-server/src/test/java/com/truward/brikar/server/test/launcher/StandardLauncherTest.java
+++ brikar-server/src/test/java/com/truward/brikar/server/test/launcher/StandardLauncherTest.java
@@ -1,6 +1,10 @@
 package com.truward.brikar.server.test.launcher;
 
+import com.truward.brikar.server.launcher.StandardLauncher;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Alexander Shabanov
@@ -8,5 +12,8 @@ import org.junit.Test;
 public class StandardLauncherTest {
 
   @Test
-  public void dummy() {}
+  public void shouldInitializeDefaults() {
+    final StandardLauncher launcher = new StandardLauncher();
+    assertEquals("classpath:/default.properties", launcher.getConfigPath());
+  }
 }
