4911d19e2c43743180bb5c20e76efdc2216e46fa
Alexander Shabanov <avshabanov@gmail.com>
2016-04-10 12:01:02 -0700

    Removing deprecated dependency on brikar-protobuf-test

---
+++ brikar-protobuf/brikar-grpc-rest/src/main/java/com/truward/brikar/grpc/ServletRpcBinding.java
@@ -0,0 +1,14 @@
+package com.truward.brikar.grpc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Alexander Shabanov
+ */
+public interface ServletRpcBinding {
+
+  void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
